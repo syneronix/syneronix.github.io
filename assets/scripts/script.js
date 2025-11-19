70f4c7adfada5475a119a0f77ec3d2d98d55f1c9
Ircman <33596277+Ircman@users.noreply.github.com>
2025-11-19 11:30:28 +0200

    updated icon load

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,45 @@
-// Добавляем favicon
+// Добавляем все иконки и манифест
+const basePath = window.location.pathname.includes("gallery") || window.location.pathname.includes("privacy") || window.location.pathname.includes("terms")
+  ? "../"
+  : "";
+
+const head = document.head;
+
 const favicon = document.createElement("link");
 favicon.rel = "icon";
 favicon.type = "image/x-icon";
-favicon.href = window.location.pathname.includes("gallery") || window.location.pathname.includes("privacy") || window.location.pathname.includes("terms")
-  ? "../favicon.ico"
-  : "favicon.ico";
-document.head.appendChild(favicon);
+favicon.href = basePath + "favicon.ico";
+head.appendChild(favicon);
+
+
+const appleTouchIcon = document.createElement("link");
+appleTouchIcon.rel = "apple-touch-icon";
+appleTouchIcon.sizes = "180x180";
+appleTouchIcon.href = basePath + "apple-touch-icon.png";
+head.appendChild(appleTouchIcon);
+
+
+const manifest = document.createElement("link");
+manifest.rel = "manifest";
+manifest.href = basePath + "site.webmanifest";
+head.appendChild(manifest);
+
 
 const viewport = document.createElement("meta");
 viewport.name = "viewport";
 viewport.content = "width=device-width, initial-scale=1.0";
-document.head.appendChild(viewport);
-
+head.appendChild(viewport);
 
 const description = document.createElement("meta");
 description.name = "Syneronix CryptoBook";
 description.content = "Syneronix CryptoBook";
-document.head.appendChild(description);
+head.appendChild(description);
 
 const fontLink = document.createElement("link");
 fontLink.href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap";
 fontLink.rel = "stylesheet";
-document.head.appendChild(fontLink);
+head.appendChild(fontLink);
+
 
 // Когда DOM готов — загружаем header и footer
 document.addEventListener("DOMContentLoaded", () => {
